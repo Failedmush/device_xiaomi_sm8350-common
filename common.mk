c8149399ac2505650e360f5bab180472932cb763
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-08-19 16:25:11 +0100

    sm8350-common: Add libaudiosystem_shim to resolve missing symbol
    Signed-off-by: Hexdare <mohammedasimuddin786@gmail.com>
    
    Introduce the libaudiosystem_shim to address the unresolved symbol:
    _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_t
    
    The symbol corresponds to the method signature:
    android::AudioSystem::setDeviceConnectionState(audio_policy_dev_state_t, android::media::audio::common::AudioPort const&, audio_format_t)
    
    This maps to AudioSystem::setDeviceConnectionState in frameworks/av/media/libaudioclient/AudioSystem.cpp:
    status_t AudioSystem::setjpDeviceConnectionState(
    audio_policy_dev_state_t state,
    const android::media::audio::common::AudioPort& port,
    audio_format_t encodedFormat,
    bool deviceSwitch)
    
    The discrepancy arises because the commit
    VoltageOS-staging/frameworks_av@7099778
    introduced a bool deviceSwitch parameter to the AudioSystem::setDeviceConnectionState method, which libwfdservice.so does not account for.
    
    About deviceSwitch (bool):
    - true: Immediately switch the audio output to the specified device.
    - false: Register the connection without automatically switching audio output.
    
    The decision has been made to leave deviceSwitch set to false as a conservative approach, given the behavior it controls

--- common.mk
+++ common.mk
@@ -392,6 +392,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Shims
+PRODUCT_PACKAGES += \
+    libcrypto-v34 \
+    libaudiosystem_shim
+
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.qti
